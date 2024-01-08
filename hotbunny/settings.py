9db2b0cfb77ff404fcb77546f096cc75d6ebf0a1
Royweru <royezi17@gmail.com>
2024-01-08 07:57:05 +0300

    modified:   hotbunny/__pycache__/settings.cpython-311.pyc 	modified:   hotbunny/__pycache__/urls.cpython-311.pyc 	modified:   hotbunny/settings.py 	modified:   hotbunny/urls.py

--- hotbunny/settings.py
+++ hotbunny/settings.py
@@ -100,8 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-MEDIA_ROOT = BASE_DIR/'media'
-MEDIA_URL = 'media/images/'
+MEDIA_ROOT = ''
+
+MEDIA_URL = ''
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
