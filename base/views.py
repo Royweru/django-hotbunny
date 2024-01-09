2a942c13c6b23a18a7ad37149921baeb573a1246
Royweru <royezi17@gmail.com>
2024-01-09 09:53:30 +0300

    modified:   base/__pycache__/views.cpython-311.pyc 	modified:   base/views.py 	modified:   hotbunny/__pycache__/settings.cpython-311.pyc 	modified:   templates/base/index.html

--- base/views.py
+++ base/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Escort
+from .models import Escort,Area
 # Create your views here.
 
 
 def index(request):
     escorts = Escort.objects.all()
-    context={'escorts':escorts}
+    locations = Area.objects.all()
+    context={'escorts':escorts,'locations':locations}
     return render(request,'base/index.html',context)
