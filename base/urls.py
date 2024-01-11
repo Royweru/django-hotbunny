2184b95b0238f4c7a6d16287153fa04bc50c4b71
Royweru <royezi17@gmail.com>
2024-01-11 20:01:50 +0300

    modified:   base/__pycache__/urls.cpython-311.pyc 	modified:   base/__pycache__/views.cpython-311.pyc 	modified:   base/urls.py 	modified:   base/views.py 	new file:   static/css/card.css 	modified:   static/css/main.css 	new file:   templates/base/escort.html 	modified:   templates/base/index.html 	modified:   templates/components/card.html 	modified:   templates/components/locations-bar.html 	modified:   templates/main.html 	modified:   templates/navbar.html

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='home'),
+    path('escort/<str:pk>/',views.individual_escort,name='view-escort')
 ]
--- base/views.py
+++ base/views.py
@@ -8,3 +8,9 @@ def index(request):
     locations = Area.objects.all()
     context={'escorts':escorts,'locations':locations}
     return render(request,'base/index.html',context)
+
+
+def individual_escort(request,pk):
+    escort = Escort.objects.get(id=pk)
+    context = {'escort':escort}
+    return render(request, 'base/escort.html',context)
