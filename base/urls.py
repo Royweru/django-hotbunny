0e61112a4303a99528479b3bcc8d968f282968c0
Royweru <royezi17@gmail.com>
2024-01-15 09:41:02 +0300

    modified:   base/__pycache__/urls.cpython-311.pyc 	modified:   base/urls.py 	modified:   db.sqlite3 	modified:   hotbunny/__pycache__/settings.cpython-311.pyc 	modified:   hotbunny/settings.py 	new file:   media/images/Dileny.jpg

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,11 @@
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('',views.index,name='home'),
     path('escort/<str:pk>/',views.individual_escort,name='view-escort')
 ]
+
